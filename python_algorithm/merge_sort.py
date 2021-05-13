56e57a3cd31e5ab7f7dfecbea589c2a96b5302ba
olive-su <1466su@naver.com>
2021-05-13 23:28:38 +0900

    0513

--- python_algorithm/merge_sort.py
+++ python_algorithm/merge_sort.py
@@ -24,6 +24,7 @@ def merge_sort(my_list):
         list2 = my_list[(len(my_list))//2:]
         return merge(merge_sort(list1), merge_sort(list2))
 
+
 print(merge_sort([1, 3, 5, 7, 9, 11, 13, 11]))
 print(merge_sort([28, 13, 9, 30, 1, 48, 5, 7, 15]))
 print(merge_sort([2, 5, 6, 7, 1, 2, 4, 7, 10, 11, 4, 15, 13, 1, 6, 4]))

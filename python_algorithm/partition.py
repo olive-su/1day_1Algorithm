a623b7b86495c564bdc57d512c382ab7ca8e27fa
olive-su <1466su@naver.com>
2021-05-16 23:20:46 +0900

    0514

---
+++ python_algorithm/partition.py
@@ -0,0 +1,22 @@
+
+def swap_elements(my_list, index1, index2):
+
+
+
+
+def partition(my_list, start, end):
+
+
+
+
+list1 = [4, 3, 6, 2, 7, 1, 5]
+pivot_index1 = partition(list1, 0, len(list1) - 1)
+print(list1)
+print(pivot_index1)
+
+
+list2 = [6, 1, 2, 6, 3, 5, 4]
+pivot_index2 = partition(list2, 0, len(list2) - 1)
+print(list2)
+print(pivot_index2)
+

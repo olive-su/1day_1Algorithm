0ba85cf809eb313c3c96c6da09e6f547dd33b333
olive-su <1466su@naver.com>
2021-06-01 13:32:14 +0900

    0531

--- python_algorithm/sequence_num.py
+++ python_algorithm/sequence_num.py
@@ -1,13 +1,13 @@
 input_num = int(input())
 cnt = 0
 flag = 1
-for j in range(1, input_num):
+for j in range(1, input_num + 1):
   sequence = list(map(int, str(j)))
-  for i in range(1, len(sequence) - 1):
+  for i in range(2, len(sequence)):
     diff = sequence[0] - sequence[1]
-    if diff == (sequence[i - 1] - sequence[i]):
+    flag = 1
+    if diff != (sequence[i - 1] - sequence[i]):
       flag = 0
       break
-    flag = 1
   if flag: cnt += 1 
 print(cnt)

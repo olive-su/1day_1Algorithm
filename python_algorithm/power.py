22478f1d9d2b6ab78eb7524cef44005132fa3a9e
olive-su <1466su@naver.com>
2021-06-03 23:08:24 +0900

    0603

---
+++ python_algorithm/power.py
@@ -0,0 +1,13 @@
+def power(x, y):
+    if y == 0:
+        return 1
+        
+    if y % 2:
+        return x * power(x, y // 2) * power(x, y // 2)
+    else:
+        return power(x, y // 2) * power(x, y // 2)
+
+
+print(power(3, 5))
+print(power(5, 6))
+print(power(7, 9))

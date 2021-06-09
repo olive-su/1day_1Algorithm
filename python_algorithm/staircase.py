4dced20766edb3b846b82a905b5e6c9705ff68fe
olive-su <1466su@naver.com>
2021-06-10 00:46:32 +0900

    0609

--- python_algorithm/staircase.py
+++ python_algorithm/staircase.py
@@ -1,17 +1,15 @@
-def calc(n):
-    if n <= 1:
-        return 1
-    else:
-        return n // 2 + calc(n - 1)
-
 def staircase(n):
-    case = 1
-    case += calc(n)
-    return case
-        
+    case = [1]
+    for i in range(1, n + 1):
+        # case[0], case[1]은 1로 설정
+        if i < 2:
+            case.append(1) 
+        else:
+            case.append(case[i - 1] + case[i - 2])
+    return case[n]
+    
 print(staircase(0))
 print(staircase(6))
 print(staircase(15))
 print(staircase(25))
 print(staircase(41))
-

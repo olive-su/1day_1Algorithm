71897ea344f98ad6cf8ad4ae7c0e29c83e98779b
olive-su <1466su@naver.com>
2021-08-21 23:56:36 +0900

    0821

--- python_algorithm/sorting_coordinate2.py
+++ python_algorithm/sorting_coordinate2.py
@@ -5,6 +5,6 @@ for _ in range(n):
     b = stdin.readline().split()
     a.append((int(b[0]), int(b[1])))
 
-a.sort(key = lambda x : x[0])
-a.sort(key = lambda x : x[1])
+a.sort(key = lambda x : x[0]) # x좌표 기준 정렬
+a.sort(key = lambda x : x[1]) # y좌표 기준 정렬
 [print('{} {}'.format(i[0], i[1])) for i in a]

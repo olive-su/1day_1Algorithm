ef4ca1640fd68c063fb9985e59b2edd796e9885f
olive_su <1466su@naver.com>
2021-09-21 18:00:51 +0900

    0920

--- python_algorithm/vowel_dic.py
+++ python_algorithm/vowel_dic.py
@@ -5,8 +5,10 @@ def solution(word):
     string = []
     while True:
         cnt += 1
+        # 문자열의 개수가 5 미만일 경우, 뒤에 문자를 추가해줌
         if len(string) < 5:
             string.append('A')
+        # 맨 뒤 문자가 'U'일 경우, 문자열 표현의 한계에 다다랐으므로 앞의 문자를 한 수준 증가함
         elif string[-1] == 'U':
             while string[-1] == 'U':
                 string.pop()

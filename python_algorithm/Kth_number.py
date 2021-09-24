1f99a1b871281b39fb4e5831a590b2045bcd01bd
olive_su <1466su@naver.com>
2021-09-24 23:49:52 +0900

    0921

---
+++ python_algorithm/Kth_number.py
@@ -0,0 +1,23 @@
+from sys import stdin
+N = int(stdin.readline())
+k = int(stdin.readline())
+arr = []
+for i in range()
+
+'''
+절반 나눠서 위에 있는지 
+아래에 있는지 판단
+
+
+아래(더 크면) y축 +1
+위(더 작으면) x축 -1
+
+짝수/홀수로 나눠서 생각
+
+n + round(n/2)
+
+pivot 값을 round(n^2 /2) - n/2값으로 잡음
+
+1.중간값 기준으로 나눠서 생각
+2.함수) 작으면 
+'''

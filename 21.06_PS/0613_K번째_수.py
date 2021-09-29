52886ab7e2330e101c584992f0284c2f8d58a0d0
olive_su <1466su@naver.com>
2021-09-30 01:48:42 +0900

    6_repo organization

--- 21.06_PS/0613_K번째_수.py
+++ 21.06_PS/0613_K번째_수.py
@@ -1,16 +1,7 @@
-N, K = int(input()), int(input())
-start, end = 1, K
-
-while start <= end:
-    cnt = 0
-    mid = (start + end) // 2
-    for i in range(1, N+1):
-        if mid//i < N: cnt += mid//i
-        else: cnt += N
-    
-    if cnt >= K: 
-        rst = mid
-        end = mid - 1
-    else:
-        start = mid + 1
-print(rst)
+def solution(array, commands):
+    answer = []
+    for i in range(len(commands)):
+        sorting_array = sorted(array[commands[i][0]-1 : commands[i][1]])
+        answer.append(sorting_array[commands[i][2]-1])
+        
+    return answer

dfe415732cf30096609b04b5bd0cb6429e197ecd
olive_su <1466su@naver.com>
2021-10-01 23:59:30 +0900

    0925

---
+++ 21.09_PS/0925_파도반_수열.py
@@ -0,0 +1,14 @@
+n = int(input())
+dp = [0 for _ in range(101)]
+
+for i in range(0, 4):       
+    dp[i] = 1 
+for j in range(4, 6):
+    dp[j] = 2
+
+for _ in range(n):
+    m = int(input())
+    for k in range(6, m+1):
+        dp[k] = dp[k-1] + dp[k-5] 
+print(dp[m])
+

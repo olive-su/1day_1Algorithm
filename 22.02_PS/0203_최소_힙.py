1318efda92723e5067a0aa5c22b813bfe7fb6502
olive-su <1466su@naver.com>
2022-02-04 23:48:09 +0900

    2022.02.03.PS

---
+++ 22.02_PS/0203_최소_힙.py
@@ -0,0 +1,13 @@
+from heapq import *
+from sys import stdin
+n = int(stdin.readline())
+heap = []
+for _ in range(n):
+    command = int(stdin.readline())
+    if command == 0:
+        if len(heap) < 1:
+            print(0)
+        else:
+            print(heappop(heap))
+    else:
+        heappush(heap, command)

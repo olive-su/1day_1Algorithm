faa8f1d589d5254c7d1b385cbc46bb1de31076f9
olive-su <1466su@naver.com>
2022-03-19 23:11:44 +0900

    2022.03.18.PS

---
+++ 22.03_PS/0318_LCS.py
@@ -0,0 +1,12 @@
+from sys import stdin
+
+string1 = stdin.readline().rstrip()
+string2 = stdin.readline().rstrip()
+dp = [[0] * (len(string2) + 1)  for _ in range(len(string1) + 1)]
+for i in range(1, len(string1) + 1): 
+    for j in range(1, len(string2) + 1): 
+        if string1[i - 1] == string2[j - 1]: 
+            dp[i][j] = dp[i - 1][j - 1] + 1 
+        else: 
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) 
+print(dp[-1][-1])

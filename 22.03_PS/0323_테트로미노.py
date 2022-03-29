d952da34c7958880b4e66168deb2cd2f9cf5bc3c
olive-su <1466su@naver.com>
2022-03-29 19:19:07 +0900

    2022.03.23.PS

---
+++ 22.03_PS/0323_테트로미노.py
@@ -0,0 +1,55 @@
+from sys import stdin
+from copy import deepcopy
+import sys
+sys.setrecursionlimit(10**7)
+
+n, m = map(int, stdin.readline().split())
+graph = []
+visited = [[0 for j in range(m)] for i in range(n)]
+v = [[0 for j in range(m)] for i in range(n)]
+global rst
+rst = 0
+for i in range(n):
+    graph.append(list(map(int, stdin.readline().split())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def dfs(x, y, step, total):
+    global rst
+    if step == 4:
+        rst = max(rst, total)
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            continue
+        if visited[nx][ny]:
+            continue    
+        visited[x][y] = 1
+        dfs(nx, ny, step + 1, total + graph[nx][ny])
+        visited[x][y] = 0
+
+
+def dfs_cross(x, y):
+    global rst
+    start = graph[x][y]
+    if x - 1 >= 0 and x + 1 < n and y - 1 >= 0:  # ㅓ
+        rst = max(rst, start + graph[x-1][y] + graph[x+1][y] + graph[x][y-1])
+    if x - 1 >= 0 and y - 1 >= 0 and y + 1 < m:  # ㅗ
+        rst = max(rst, start + graph[x-1][y] + graph[x][y-1] + graph[x][y+1])
+    if x - 1 >= 0 and x + 1 < n and y + 1 < m:  # ㅏ
+        rst = max(rst, start + graph[x-1][y] + graph[x+1][y] + graph[x][y+1])
+    if x + 1 < n and y - 1 >= 0 and y + 1 < m:  # ㅜ
+        rst = max(rst, start + graph[x+1][y] + graph[x][y-1] + graph[x][y+1])
+
+
+for i in range(n):
+    for j in range(m):
+        visited = deepcopy(v)  # visited 초기화
+        dfs(i, j, 1, graph[i][j])
+        dfs_cross(i, j)
+
+print(rst)
+

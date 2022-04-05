1ec202271874b559fb409cbdd29af575e1808985
olive-su <1466su@naver.com>
2022-04-05 23:24:24 +0900

    2022.03.30.PS

---
+++ 22.03_PS/0330_N-Queen.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+ans = 0
+row = [0] * n
+
+def backtracking(x):
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x] - row[i]) == abs(x - i):
+            return False
+    return True
+
+def n_queens(x):
+    global ans
+    if x == n:
+        ans += 1
+    else:
+        for i in range(n):
+            row[x] = i
+            if backtracking(x):
+                n_queens(x+1)
+n_queens(0)
+print(ans)

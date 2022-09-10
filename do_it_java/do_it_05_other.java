ae4736c4ea9b6060cf69bada9820ce0365145d37
olive-su <1466su@naver.com>
2022-09-10 23:39:41 +0900

    2022.09.10.PS

---
+++ do_it_java/do_it_05_other.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class do_it_05_other {
+    public static void main(String args[]) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int m = Integer.parseInt(stringTokenizer.nextToken());
+        long[] sumArr = new long[n];
+        long[] countArr = new long[m];
+        long answer = 0;
+
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        sumArr[0] = Long.parseLong(stringTokenizer.nextToken());
+        for (int i = 1; i < n; i++) {
+            sumArr[i] = sumArr[i - 1] + Long.parseLong(stringTokenizer.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            countArr[(int)(sumArr[i] % m)]++;
+        }
+        answer += countArr[0];
+
+        for (int i = 0; i < m; i++) {
+            if (countArr[i] > 1) {
+                answer += countArr[i] * (countArr[i] - 1) / 2;
+            }
+        }
+        System.out.println(answer);
+    }
+}

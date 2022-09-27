76d9c63aa97ad51ea3f6a56b81e7dea1c5c45e1d
olive-su <1466su@naver.com>
2022-09-27 23:03:34 +0900

    2022.09.07.PS

---
+++ 22.09_PS/0907_평균.java
@@ -0,0 +1,24 @@
+// boj.1546
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        double sum = 0.0;
+        long max = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] > max)
+                max = a[i];
+        }
+        for (int i = 0; i < n; i++) {
+            sum += (double)a[i] / max * 100;
+        }
+        System.out.print(sum / n);
+    }
+}

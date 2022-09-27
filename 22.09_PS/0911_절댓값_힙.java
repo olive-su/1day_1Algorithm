4751cf5fb1c25c8c296c298ccb2d37d7834d11a8
olive-su <1466su@naver.com>
2022-09-27 23:03:59 +0900

    2022.09.11.PS

---
+++ 22.09_PS/0911_절댓값_힙.java
@@ -0,0 +1,32 @@
+// boj. 11286
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
+            int fAbs = Math.abs(o1);
+            int sAbs = Math.abs(o2);
+            if (fAbs == sAbs) {
+                return o1 > o2 ? 1 : -1;
+            } else {
+                return fAbs - sAbs;
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            int command = Integer.parseInt(br.readLine());
+            if (command == 0) {
+                if (queue.isEmpty())
+                    System.out.println(0);
+                else
+                    System.out.println(queue.poll());
+            } else {
+                queue.add(command);
+            }
+        }
+    }
+}
---
+++ 22.09_PS/0911_카드2.java
@@ -0,0 +1,20 @@
+// boj. 2164
+
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.remove();
+            queue.add(queue.remove());
+        }
+        System.out.println(queue.remove());
+    }
+}

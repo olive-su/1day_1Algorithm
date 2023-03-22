b75320f59f1ce1f527ca8b33a2b3aeab38ff094b
olive-su <1466su@naver.com>
2023-03-23 00:31:56 +0900

    2023.03.22.PS

--- 23.03_PS/0322_작업.py
+++ 23.03_PS/0322_작업.py
@@ -5,9 +5,9 @@ input = sys.stdin.readline
 
 n = int(input())
 graph, indegree = [[] for _ in range(n + 1)], [0] * (n + 1)
-time = [[] for _ in range(n + 1)] 
-queue = deque([])
-answer = 0
+time = [0] * (n + 1) 
+dp = [0] * (n + 1) 
+queue = deque()
 
 for i in range(1, n + 1):
     arr = list(map(int, input().split()))
@@ -18,19 +18,17 @@ for i in range(1, n + 1):
             indegree[i] += 1
 
 for i in range(1, n + 1):
-    if indegree[i] == 0: 
+    if indegree[i] == 0:
         queue.append(i)
+        dp[i] = time[i]
 
 while queue:
     i = queue.popleft()
     for j in graph[i]:
         indegree[j] -= 1
+        dp[j] = max(dp[i] + time[j], dp[j])
         if indegree[j] == 0: 
             queue.append(j)
-            time[j] += time[i]
-            
 
-print(max(time[1:]))
-
-
+print(max(dp))
 

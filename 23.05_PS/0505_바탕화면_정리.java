77cecab0b4c4ff22b7d426f0eeb27cfb855ecc77
olive-su <1466su@naver.com>
2023-05-05 18:44:50 +0900

    2023.05.05.PS

---
+++ 23.05_PS/0505_바탕화면_정리.java
@@ -0,0 +1,27 @@
+// time: 10'
+
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        
+        int lux = Integer.MAX_VALUE;
+        int luy = Integer.MAX_VALUE;
+        int rdx = Integer.MIN_VALUE;
+        int rdy = Integer.MIN_VALUE;
+        
+        
+        for(int i = 0; i < wallpaper.length; i++){
+            for(int j = 0; j < wallpaper[0].length(); j++){
+                if(wallpaper[i].charAt(j) == '#'){
+                    lux = Math.min(lux, i);
+                    luy = Math.min(luy, j);
+                    rdx = Math.max(rdx, i);
+                    rdy = Math.max(rdy, j);
+                }
+            }
+        }
+        
+        
+        int[] answer = {lux, luy, rdx + 1, rdy + 1};
+        return answer;
+    }
+}
